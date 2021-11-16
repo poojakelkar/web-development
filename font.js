42fad03ec49ff1c563036ede35fd92e3b4a40f59
Pooja Kelkar <poojakelkar53@gmail.com>
2021-11-16 17:17:44 +0530

    font increases button

---
+++ font.js
@@ -0,0 +1,21 @@
+var added = document.getElementById('add')
+var substract = document.getElementById('sub')
+
+
+function adding(){
+    var text = document.getElementById('change')
+    console.log(text)
+}
+
+
+
+function sub(){
+    var font = document.getElementById('change')
+    console.log(font)
+
+//   document.getElementById('change'). style. fontSize = "0rem"
+  }
+
+
+added.addEventListener('click',adding)
+substract.addEventListener('click',sub)
