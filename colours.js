45d28ce844b4c42876ee7f8e55e1f0e69fb8c0bc
Pooja Kelkar <poojakelkar53@gmail.com>
2021-11-21 19:37:40 +0530

    changes are done

--- colours.js
+++ colours.js
@@ -7,7 +7,7 @@ const arr = ['blue', 'green', 'yellow', 'red']
 
 function showColours(){
     for(let i=0;i<arr.length;i++){
-        list.innerHTML += '<li style= "background:' +arr[i] +'">' + arr[i] +'</li>'
+        list.innerHTML =list.innerHTML + '<li style= "background:' +arr[i] +'">' + arr[i] +'</li>'
     }
 }
 
