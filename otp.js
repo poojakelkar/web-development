257799a5ebde2be8985e6be8c92a73451aae7e1a
Pooja Kelkar <poojakelkar53@gmail.com>
2021-11-23 19:29:48 +0530

    otpgenerator done

--- otp.js
+++ otp.js
@@ -1,21 +1,18 @@
 var userName = document.getElementById("otp")
 var submit = document.getElementById("submit")
-var output = document.getElementById("output")
+var output = document.getElementById("outputValue")
 
 
-function response(){
-
-fetch(
-    'https://otpgenerator.ishanjirety.repl.co/get-otp/?name=' + userName.value
+submit.addEventListener('click',function (){
+  fetch(
+    'https://otpgenerator.ishanjirety.repl.co/get-otp/?name=' +userName.value
   )
-    .then(function (res) {
-      return res.json
-    })
-    .then(function (res) {
-      console.log(res)
-      output.innerText = res.otp
-    })
+  .then(function(res){
+    return res.json()
+  })
+  .then(function(res){
+    console.log(res)
+    output.innerText = res.otp
+  })
 
-}
-
-submit.addEventListener('click',response)
+})
